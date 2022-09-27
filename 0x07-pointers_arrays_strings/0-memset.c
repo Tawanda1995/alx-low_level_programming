8a4d9c7052fd10e786e42e35b16e26179e6ba0d6
Tawanda1995 <mupfunyat@gmail.com>
2022-09-26 19:33:32 -0700

    more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,39 @@
-#include "main.h"
+#include "main.h"                                                                                                                                        
 
-/**
-* _memset - fills a memory block with a constant byte
-* @s:address to memory block
-* @n : number of bytes to be used
-* 2b: char to be used
-*
-* Return: pointer to the memory
-*/
+                                                                                                                                                         
 
-char *_memset(char *s, char b, unsigned int n)
-{
-	while (n)
-	{
-		s[n-1] = b;
-		n--;
-	}
-	return (s);
+/**                                                                                                                                                      
+
+ * _memset - fills a memory block with a constant byte                                                                                                    
+
+ * @s: address to memory block                                                                                                                             
+
+ * @n: number of bytes to be used                                                                                                                        
+
+ * @b: char to be used                                                                                                                                    
+
+ *                                                                                                                                                        
+
+ * Return: pointer to the memory                                                                                                                          
+
+ */                                                                                                                                                       
+
+                                                                                                                                                         
+
+char *_memset(char *s, char b, unsigned int n)                                                                                                           
+
+{                                                                                                                                                        
+
+        while (n)                                                                                                                                        
+
+        {                                                                                                                                                
+
+                s[n-1] = b;                                                                                                                              
+
+                n--;                                                                                                                                     
+
+        }                                                                                                                                                
+
+        return (s);                                                                                                                                      
+
 }
