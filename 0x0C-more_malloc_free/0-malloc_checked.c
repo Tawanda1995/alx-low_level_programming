e3c3bd39f360cb432d6fbc9cadef5068b6464c9b
Tawanda1995 <mupfunyat@gmail.com>
2022-10-06 12:11:20 -0700

    More malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,11 @@
  * 
  * Return: pointer to allocated memory or normal process termination if error
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-
+	
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
